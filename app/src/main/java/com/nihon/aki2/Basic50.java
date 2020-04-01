79829f04fe25726d1e9907b62741671a7e96f0db
cool3690 <cool3690@yahoo.com.tw>
2020-04-01 09:02:26 +0800

    0401

--- app/src/main/java/com/nihon/aki2/Basic50.java
+++ app/src/main/java/com/nihon/aki2/Basic50.java
@@ -1,5 +1,6 @@
 package com.nihon.aki2;
 
+import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
@@ -9,15 +10,19 @@ import com.nihon.aki2.mydb.dbbasic50;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
 
 import android.os.StrictMode;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,11 +30,15 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Basic50 extends AppCompatActivity {
     private PaintView paintView;
     ImageView clean,sound,img,btnpre,btnnext;
     TextView show;
+
+    GifImageView mygif;
     int num=0;
     public MediaPlayer mediaplayer;
     int[] songfile=new int[] {R.raw.a, R.raw.i, R.raw.u, R.raw.e, R.raw.o,R.raw.ka, R.raw.ki, R.raw.ku, R.raw.ke, R.raw.ko,R.raw.sa, R.raw.shi, R.raw.su, R.raw.se, R.raw.so,
@@ -43,6 +52,9 @@ public class Basic50 extends AppCompatActivity {
     int[] pic2=new int[] {R.drawable.a_1, R.drawable.i_1, R.drawable.u_1, R.drawable.e_1, R.drawable.o_1,R.drawable.ka_1, R.drawable.ki_1, R.drawable.ku_1, R.drawable.ke_1, R.drawable.ko_1,R.drawable.sa_1, R.drawable.shi_1, R.drawable.su_1, R.drawable.se_1, R.drawable.so_1,
             R.drawable.ta_1, R.drawable.chi_1, R.drawable.tsu_1, R.drawable.te_1, R.drawable.to_1,R.drawable.na_1, R.drawable.ni_1, R.drawable.nu_1, R.drawable.ne_1, R.drawable.no_1,R.drawable.ha_1, R.drawable.hi_1, R.drawable.fu_1, R.drawable.he_1, R.drawable.ho_1,R.drawable.ma_1, R.drawable.mi_1, R.drawable.mu_1,
             R.drawable.me_1, R.drawable.mo_1,R.drawable.ya_1, R.drawable.yu_1, R.drawable.yo_1,R.drawable.ra_1, R.drawable.ri_1, R.drawable.ru_1, R.drawable.re_1, R.drawable.ro_1,R.drawable.wa_1,R.drawable.n_1,R.drawable.n_1};
+    int[] picg=new int[] {R.drawable.a_g, R.drawable.i_g, R.drawable.u_g, R.drawable.e_g, R.drawable.o_g,R.drawable.ka_g, R.drawable.ki_g, R.drawable.ku_g, R.drawable.ke_g, R.drawable.ko_g,R.drawable.sa_g, R.drawable.shi_g, R.drawable.su_g, R.drawable.se_g, R.drawable.so_g,
+            R.drawable.ta_g, R.drawable.chi_g, R.drawable.tsu_g, R.drawable.te_g, R.drawable.to_g,R.drawable.na_g, R.drawable.ni_g, R.drawable.nu_g, R.drawable.ne_g, R.drawable.no_g,R.drawable.ha_g, R.drawable.hi_g, R.drawable.fu_g, R.drawable.he_g, R.drawable.ho_g,R.drawable.ma_g, R.drawable.mi_g, R.drawable.mu_g,
+            R.drawable.me_g, R.drawable.mo_g,R.drawable.ya_g, R.drawable.yu_g, R.drawable.yo_g,R.drawable.ra_g, R.drawable.ri_g, R.drawable.ru_g, R.drawable.re_g, R.drawable.ro_g,R.drawable.wa_g,R.drawable.n_g,R.drawable.wo_g};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +73,7 @@ public class Basic50 extends AppCompatActivity {
                 .penaltyLog()
                 .penaltyDeath()
                 .build());
+        mygif=(GifImageView)findViewById(R.id.mygif);
         paintView = (PaintView) findViewById(R.id.paintView);
         clean=(ImageView)findViewById(R.id.clean);
         sound=(ImageView)findViewById(R.id.sound);
@@ -75,12 +88,37 @@ public class Basic50 extends AppCompatActivity {
         mediaplayer=new MediaPlayer();
         clean.setOnClickListener(cbtn);
         sound.setOnClickListener(sbtn);
+
         pre();
         //show.setText("あい（愛）"+"\n[a-i]\n"+"愛：愛慕");
         btnpre.setOnTouchListener(btpre);
         btnnext.setOnTouchListener(btnext);
+      //  init();
+    }
+    private void init() {
+        mygif.setVisibility(View.VISIBLE);
+        mygif.setImageResource(picg[num]);
+        final GifDrawable getDura=GifDrawable.createFromResource(getResources(),picg[num]);
+        int duration=getDura.getDuration();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+               runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //getDura.stop();
+                    mygif.setVisibility(View.GONE);
+                    paintView.setVisibility(View.VISIBLE);
+
+                    }
+                });
+            }
+        }, duration);
 
     }
+
+
     private ImageView.OnClickListener cbtn=new ImageView.OnClickListener(){
         @Override
         public void onClick(View v) {
@@ -109,6 +147,8 @@ public class Basic50 extends AppCompatActivity {
     }
     public void pre(){
         int s=num+1;
+
+        paintView.setVisibility(View.INVISIBLE);
         String result = dbbasic50.executeQuery(s+"");
 
         try{
@@ -126,6 +166,7 @@ public class Basic50 extends AppCompatActivity {
         }
 
         catch(Exception e){}
+        init();
 
     }
     private ImageView.OnTouchListener btnext=new ImageView.OnTouchListener(){
