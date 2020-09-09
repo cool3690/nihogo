1428cd05dc10881b43e9f24c782b70da34f71b4e
cool3690 <cool3690@yahoo.com.tw>
2020-09-09 15:37:33 +0800

    0909 Frag3.java

--- app/src/main/java/com/nihon/aki2/Frag3.java
+++ app/src/main/java/com/nihon/aki2/Frag3.java
@@ -6,13 +6,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
+
+import com.nihon.aki2.control.VideoAdapter;
+import com.nihon.aki2.control.YouTubeVideos;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Frag3 extends Fragment {
     Button bt1,bt2;
+    WebView videoWebView;
+    RecyclerView recyclerView;
+    private int secondLeft = 7;
+    Timer timer = new Timer();
+    Vector<YouTubeVideos> youtubeVideos = new Vector<YouTubeVideos>();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +42,55 @@ public class Frag3 extends Fragment {
         bt1.setOnClickListener(btlink);
         bt2=(Button)view.findViewById(R.id.bt2);
         bt2.setOnClickListener(btlink2);
+
+
+        videoWebView=(WebView)view.findViewById(R.id.videoWebView);
+        recyclerView = (RecyclerView)view.findViewById(R.id.recyclerView);
+        recyclerView.setVisibility(View.GONE);
+        videoWebView.setVisibility(View.GONE);
+         begin();
         return view;
        // return inflater.inflate(R.layout.frag2_layout, container, false);
+    }
+
+    public void begin() {
+        timer.schedule(task, 1000, 1000) ;       }
+
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // TODO Auto-generated method stub
+                    secondLeft--;
+
+                    if (secondLeft==0) {
+                        play();
+
+                    }
+                }
+            });
+        }
+    };
+    public void play(){
+
+        videoWebView.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.VISIBLE);
+
+        videoWebView.bringToFront();
+        recyclerView.bringToFront();
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager( new LinearLayoutManager(getActivity()));
+
+        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://akkyschool.com/images/study_abroad/N3-4.mp4\" frameborder=\"0\" allowfullscreen></iframe>") );
+
+        VideoAdapter videoAdapter = new VideoAdapter(youtubeVideos);//https://www.youtube.com/watch?v=xmkqU_M21lk&feature=youtu.be
+
+        recyclerView.setAdapter(videoAdapter);
+
+         /* */
     }
     private Button.OnClickListener btlink=new Button.OnClickListener(){
         @Override
