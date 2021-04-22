d85e729fc5373158f6510e6f44fd17ee1a254280
cool3690 <cool3690@yahoo.com.tw>
2021-04-22 15:41:35 +0800

    0422 clear cache

--- app/src/main/java/com/nihon/aki2/Mymenu.java
+++ app/src/main/java/com/nihon/aki2/Mymenu.java
@@ -17,6 +17,7 @@ import android.os.StrictMode;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +25,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
 
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -68,7 +71,7 @@ public class Mymenu extends AppCompatActivity {
         else{
            // schedulejob(h);
         }
-
+        clearApplicationData();
 
     }
     public void schedulejob(String tmp){
@@ -128,7 +131,32 @@ public class Mymenu extends AppCompatActivity {
             return true;
         }
     };
+    public void clearApplicationData() {
+        File cache = getCacheDir();
+        File appDir = new File(cache.getParent());
+        if(appDir.exists()){
+            String[] children = appDir.list();
+            for(String s : children){
+                if(!s.equals("lib")){
+                    deleteDir(new File(appDir, s));
+                    Log.i("TAG", "**************** File/data/data/APP_PACKAGE/" + s +" DELETED *******************");
+                }
+            }
+        }
+    }
+    public static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
 
+        return dir.delete();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
